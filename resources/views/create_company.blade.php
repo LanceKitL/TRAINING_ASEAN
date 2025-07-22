062f8b8d48327ed4c17b0e7929ef2b802b16ea22
LanceKitL <lancegomoa@gmail.com>
2025-07-22 16:45:26 +0800

    wow

--- resources/views/create_company.blade.php
+++ resources/views/create_company.blade.php
@@ -3,8 +3,26 @@
 
     <form method="POST" action="{{ route('createCompany') }}">
         @csrf
-
-
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div>
+            <label for=""></label>
+            <input type="text" name="" id="">
+        </div>
+        <div>
+            <label for=""></label>
+            <input type="text" name="" id="">
+        </div>
+        <div>
+            <label for=""></label>
+            <input type="text" name="" id="">
+        </div>
+        <div>
+            <label for=""></label>
+            <input type="text" name="" id="">
+        </div>
     </form>
 
 
